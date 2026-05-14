3a0c474f7af4c4504308235dc5dcc7d4150807f8
Tobia <random-mail@example.com>
2026-05-14 16:34:15 +0200

    stats update

--- src/data/gear/hiyuki.ts
+++ src/data/gear/hiyuki.ts
@@ -6,15 +6,12 @@ const hiyuki_weapon = buildWeapon(weaponCatalog.find(w => w.name === 'Frostburn'
 
 // ========== Echoes ===========================================================================================================
 const SET = 'Wishes of Quiet Snowfall'
-const SUBS = { critRate: 0.075, critDamage: 0.15, bonusATK: 0.079 }
-const SUBS0 = { critRate: 0.000, critDamage: 0.000, bonusATK: 0.000, liberationBonusDMG: 0.000 }
 
 const SUBS1 = { critRate: 0.063, critDamage: 0.138, bonusATK: 0.079, liberationBonusDMG: 0.071 }
 const SUBS2 = { critRate: 0.069, critDamage: 0.21,  bonusATK: 0.101, liberationBonusDMG: 0.094 }
-const SUBS3 = { critRate: 0.105, critDamage: 0.15,                   liberationBonusDMG: 0.086 }
+const SUBS3 = { critRate: 0.105, critDamage: 0.15,  flatATK: 60,     liberationBonusDMG: 0.086 }
 const SUBS4 = { critRate: 0.099, critDamage: 0.21,                                             }
-const SUBS5 = { critRate: 0.069, critDamage: 0.138,                  liberationBonusDMG: 0.086 }
-
+const SUBS5 = { critRate: 0.069, critDamage: 0.21                                              }
 
 const hiyuki_cost_4_echo_1 = buildEcho(SET, 'Aleph-1',          'bonusATK',       0.33, SUBS1)
 const hiyuki_cost_3_echo_1 = buildEcho(SET, 'Windlash Coleoid', 'bonusATK',       0.30, SUBS2)
